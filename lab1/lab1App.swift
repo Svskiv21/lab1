35ce0963be9011a1f87823c09baacf53eb5ed00a
Natalia Krukar <natalia.krukar@gmail.com>
2023-10-03 16:32:41 +0200

    Initial Commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by Natalia Krukar on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
